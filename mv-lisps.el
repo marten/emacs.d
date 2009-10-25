2a9aa363a026feebd156ff9733b19faa84006f97
Marten Veldthuis <marten@veldthuis.com>
2009-10-25 10:35:40 +0100

    slime and swank

--- mv-lisps.el
+++ mv-lisps.el
@@ -2,42 +2,11 @@
 (add-to-list 'load-path "~/.emacs.d/clojure-mode")
 (require 'clojure-mode)
 
-(add-to-list 'load-path "~/.emacs.d/swank-clojure/src/emacs")
-(setq swank-clojure-jar-path "~/.clojure/clojure.jar"
-      swank-clojure-extra-classpaths (list "~/.emacs.d/swank-clojure/src/main/clojure"
+(add-to-list 'load-path "~/.emacs.d/swank-clojure")
+(setq swank-clojure-extra-classpaths (list "~/.emacs.d/swank-clojure/src/main/clojure"
 					   "~/.clojure/clojure-contrib.jar"))
 
 (require 'swank-clojure-autoload)
-
-(defun clojure-project (path)
-  "Setup classpaths for a clojure project and starts a new SLIME session.
-Kills existing SLIME session, if any."
-  (interactive (list
-                (ido-read-directory-name
-                 "Project root: "
-                 (locate-dominating-file default-directory "src"))))
-  (require 'swank-clojure)
-  (require 'slime)
-  (when (get-buffer "*inferior-lisp*")
-    (kill-buffer "*inferior-lisp*"))
-  (add-to-list 'swank-clojure-extra-vm-args
-               (format "-Dclojure.compile.path=%s"
-                       (expand-file-name "target/classes/" path)))
-  (setq swank-clojure-binary nil
-        swank-clojure-jar-path (expand-file-name "target/dependency/" path)
-        swank-clojure-extra-classpaths
-        (append (mapcar (lambda (d) (expand-file-name d path))
-                        '("src/" "target/classes/" "test/"))
-                (let ((lib (expand-file-name "lib" path)))
-                  (if (file-exists-p lib)
-                      (directory-files lib t ".jar$"))))
-        slime-lisp-implementations
-        (cons `(clojure ,(swank-clojure-cmd) :init swank-clojure-init)
-              (remove-if #'(lambda (x) (eq (car x) 'clojure))
-                         slime-lisp-implementations)))
-  (message "Deprecated: use swank-clojure-project from swank-clojure.")
-  (save-window-excursion
-    (slime)))
 
 ;; slime
 
--- mv-misc.el
+++ mv-misc.el
@@ -127,10 +127,10 @@
   ;; Work around a bug on OS X where system-name is FQDN
   (setq system-name (car (split-string system-name "\\.")))
   ;; Work around a bug where environment variables aren't set correctly
-  (require 'osx-plist)
-  (when (file-exists-p "~/.MacOSX/environment.plist")
-    (osx-plist-update-environment)))
- 
+  ;;(require 'osx-plist)
+  ;;(when (file-exists-p "~/.MacOSX/environment.plist")
+  ;;  (osx-plist-update-environment)))
+ )
 ;; make emacs use the clipboard
 (setq x-select-enable-clipboard t)
 
