c9812bfbf8634a7f57fbce39d8b781b1c1a1911e
Marten Veldthuis <marten@veldthuis.com>
2012-04-22 20:39:07 +0200

    Add a bunch more core tweaks

--- init/init-core.el
+++ init/init-core.el
@@ -1,5 +1,20 @@
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(setq make-backup-files nil)
+;; only turn off menus if not osx
+(if (not (eq system-type 'darwin))
+    (menu-bar-mode -1))
+
+(column-number-mode t)
+(setq fill-column 85)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+
+(push (expand-file-name "/usr/local/bin") exec-path)
+(push (expand-file-name "~/bin") exec-path)
 
 (provide 'init-core)
